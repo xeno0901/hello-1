2fd04bc39c964b7e54a69633a51e24abb4e98dac
ByungDae Sohn <miconblog@gmail.com>
2019-01-28 17:43:16 +0900

    2일차 오전 강의 예제

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick() {
+  console.log('tick', this);
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const nTimer = setInterval(tick, 1000);
+
+const nTimer2 = setTimeout(function() {
+  clearInterval(nTimer);
+}, 5000);
+console.log(nTimer, nTimer2);
