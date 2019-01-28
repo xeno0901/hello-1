de8b16ed5e4612dfecb5fa555a06c2fa9fb0a253
ByungDae Sohn <miconblog@gmail.com>
2019-01-28 15:32:51 +0900

    커밋 연습

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          hghhhh
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
