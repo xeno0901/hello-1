668519de72178656c8bec9b7bc08d51224d283d4
ByungDae Sohn <miconblog@gmail.com>
2019-01-31 22:27:07 +0900

    마크다운 예제 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import Header from './components/Header';
+import MarkEditor from './components/MarkEditor';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <MarkEditor />
       </div>
     );
   }
--- src/components/MarkEditor.jsx
+++ src/components/MarkEditor.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import MarkdownIt from 'markdown-it';
+import './MarkEditor.less';
+
 const md = new MarkdownIt();
+
 const Preview = props => {
   return (
     <div
+      className={'Preview'}
       dangerouslySetInnerHTML={{
         __html: md.render(props.value),
       }}
@@ -13,7 +17,16 @@ const Preview = props => {
 
 class MarkEditor extends React.Component {
   state = {
-    value: '',
+    value: `# 마크다운 에디터 만들기
+      
+      미리보기 영역
+          this is code!
+    
+      ## 이것은 리스트
+       
+       * 리스트1
+       * 리스트2
+    `,
   };
 
   handleTextChange = ({target}) => {
@@ -24,10 +37,10 @@ class MarkEditor extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>마크다운 에디터</h2>
+      <div className={'MarkEditor'}>
+        <h2>마크다운 에디터 예제</h2>
         <Preview value={this.state.value} />
-        <textarea onChange={this.handleTextChange} />
+        <textarea className={'Editor'} onChange={this.handleTextChange} />
       </div>
     );
   }
